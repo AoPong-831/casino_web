2f5d066baaecd5baf693516a854e737c3df0f5ca
uSagi-0225 <newipadmail114514@gmail.com>
2025-09-17 03:06:07 +0900

    いったんflask migrate できるようになった

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import os
 from flask_migrate import Migrate
-import csv#csv用
 from flask import session#ログイン機能用
 from werkzeug.utils import secure_filename#日程表_画像読み込み用(危険なファイル名{../../}などを除去する)
 
@@ -15,7 +14,7 @@ login_manager = LoginManager()
 def create_app():
     #Flaskアプリ作成
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///local.db")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///app.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.secret_key = "your-secret-key" #これないとエラー出るらしい
 
@@ -26,7 +25,10 @@ def create_app():
 
     # モデルを必ずインポートして Alembic に認識させる(???)
     #⇒インポートはここでする。(循環を避けるため関数内で)
-    from app.models import User, Ticket, Chip_log
+    with app.app_context():#★ app context 内でモデルを読み込み
+        from app import models
+    
+    from app.models import User#login_manager用
 
     # user_loaderをここに書く(???)
     #ユーザー読み込み関数
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 # run.py
-from app import create_app
+from app import create_app, db
 
 app = create_app()
 
